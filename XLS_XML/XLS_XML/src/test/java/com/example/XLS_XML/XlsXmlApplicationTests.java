c3f02c7693ecd9617b889f25625b9bcbac29375d
jorgeagenjo <154449333+jorgeagenjo@users.noreply.github.com>
2024-01-31 21:45:03 +0100

    Conversion XLS a XML
    Conversion XLS a XML

---
+++ XLS_XML/XLS_XML/src/test/java/com/example/XLS_XML/XlsXmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.XLS_XML;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XlsXmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
